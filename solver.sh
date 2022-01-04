b180748d8589525186e9a4f729e4f74c9aaecff2
Park Min Hyeok <pmh_only@pmh.codes>
2022-01-04 18:30:58 +0900

    feat: reduce judge time by using async
    Signed-off-by: Park Min Hyeok <pmh_only@pmh.codes>

--- solver.sh
+++ solver.sh
@@ -1,47 +1,20 @@
 #!/bin/sh
 
-# permission correction
 chown -R root:root /judge
 chmod -R 400 /judge
 
-# build source code
 gcc source.c -o source
 
-if [ $? -eq 0 ]; then
-  printf "\n::$1:compile:success::\n"
-else
-  printf "\n::$1:compile:fail::\n"
-  exit 0
+if [ $? -ne 0 ]; then
+  printf "::$1:fail:compile::"
+  exit
 fi
 
-# find test cases & run
+judge() {
+  su-exec solver timeout $3 /source < /judge/$1.in > /judge/$1.tmp
+  cmp -s /judge/$1.tmp /judge/$1.out || printf "::$2:fail:test:$n::"
+}
+
 for n in $(ls -1 /judge | awk -F. '$2=="in"{print$1}'); do
-  printf "\n::$1:test:$n:start::\n"
-
-  # run
-  su-exec solver timeout $2 /source < /judge/$n.in | tee $n.tmp
-
-  # append LF if not exist
-  xxd $n.tmp -p | grep -qE "0a"
-
-  if [ $? -ne 0 ]; then
-    echo -ne '\x0a' >> $n.tmp
-  fi
-  
-  # append LF if not exist
-  xxd /judge/$n.out -p | grep -qE "0a"
-
-  if [ $? -ne 0 ]; then
-    echo -ne '\x0a' >> /judge/$n.out
-  fi
-
-
-  # check result
-  cmp -s $n.tmp /judge/$n.out
-
-  if [ $? -eq 0 ]; then
-    printf "\n::$1:test:$n:success::\n"
-  else
-    printf "\n::$1:test:$n:fail::\n"
-  fi
+  judge $n $1 $2 &
 done
