7ab9596ceb0ee8a0dfff65630eaa39056cafba30
Park Min Hyeok <pmh_only@pmh.codes>
2022-01-05 14:09:46 +0900

    feat: add lf to solver message
    Signed-off-by: Park Min Hyeok <pmh_only@pmh.codes>

--- solver.sh
+++ solver.sh
@@ -6,13 +6,13 @@ chmod -R 400 /judge
 gcc source.c -o source
 
 if [ $? -ne 0 ]; then
-  printf "::$1:fail:compile::"
+  printf "\n::$1:fail:compile::\n"
   exit
 fi
 
 judge() {
   su-exec solver timeout -s 9 $3 /source < /judge/$1.in > /judge/$1.tmp
-  cmp -s /judge/$1.tmp /judge/$1.out || printf "::$2:fail:test:$n::"
+  cmp -s /judge/$1.tmp /judge/$1.out || printf "\n::$2:fail:test:$n::\n"
 }
 
 for n in $(ls -1 /judge | awk -F. '$2=="in"{print$1}'); do
